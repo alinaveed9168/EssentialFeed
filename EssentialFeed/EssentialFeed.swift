8b9250511f93a9011978f3bd14bfd19db7974565
ali naveed <alinaveed9168@gmail.com>
2026-05-21 13:26:12 +0400

    Initial Commit

---
+++ EssentialFeed/EssentialFeed.swift
@@ -0,0 +1,9 @@
+//
+//  EssentialFeed.swift
+//  EssentialFeed
+//
+//  Created by ali naveed on 21/05/2026.
+//
+
+import Foundation
+
---
+++ FeedItem.swift
@@ -0,0 +1,14 @@
+//
+//  FeedItem.swift
+//  EssentialFeed
+//
+//  Created by ali naveed on 21/05/2026.
+//
+import Foundation
+
+struct FeedItem {
+    let id:UUID
+    let description:String?
+    let location:String?
+    let imageURL:String
+}
