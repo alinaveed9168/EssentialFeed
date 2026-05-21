a44b49e377dc13eb1d475ef492ef94c9bdc59034
ali naveed <alinaveed9168@gmail.com>
2026-05-21 14:57:33 +0400

    Initial commit

---
+++ EssentialFeedTests/RemoteFeedLoaderTest.swift
@@ -0,0 +1,23 @@
+//
+//  RemoteFeedLoaderTest.swift
+//  EssentialFeed
+//
+//  Created by ali naveed on 21/05/2026.
+//
+
+import XCTest
+
+class RemoteFeedLoader {
+    
+}
+
+class httpClient {
+    var requestURL: URL?
+}
+
+class RemoteFeedLoaderTest {
+    
+    func test_init() {
+        
+    }
+}
